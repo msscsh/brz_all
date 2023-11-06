e16f9a606ba3eec4dfc844e567e9f92a3dd42ecc
msscsh <itsbruno42@gmail.com>
2023-11-05 22:16:28 -0300

    mudanças iniciais

--- app/index.js
+++ app/index.js
@@ -1,10 +1,48 @@
-import { View, Text } from "react-native";
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, PopularBrawlers, ScreenHeaderBtn, Welcome } from '../components'
+
 
 const Home = () => {
+
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+                    ),
+                    headerTitle: ""
+                }}
+            />
+
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View
+                style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}
+            >
+                <Welcome
+                />
+
+                <PopularBrawlers />
+                <Nearbyjobs />
+
+            </View>
+        </ScrollView>
+
+        </SafeAreaView>
     )
 }
 
