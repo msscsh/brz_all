c337ec79c4e54029a2635467b15508a8688cf02a
msscsh <itsbruno42@gmail.com>
2023-11-19 16:49:54 -0300

    removendo projeto antigo para reestruturação #1

--- hook/useFetch.js
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const useFetch = () => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-}
