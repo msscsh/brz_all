f2347295029ce1685bbf4c1a46f0256f631e2222
msscsh <itsbruno42@gmail.com>
2023-11-26 14:21:49 -0300

    treinando layout

--- scripts/home.js
+++ scripts/home.js
@@ -1,20 +1,22 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-document.querySelector("h1").onmouseover = event => {
+function onPageLoad() {
   let iterations = 0;
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText.split("")
+
+    document.getElementById('title').innerText = "BRAZILIAN STORM".split("")
       .map((letter, index) => {
         if(index < iterations) {
-          return event.target.dataset.value[index];
+          return document.getElementById('title').dataset.value[index];
         }
         return letters[Math.floor(Math.random() * 26)]
       })
       .join("");
 
-      if(iterations >= 15)
+      if(iterations >= 15) {
         clearInterval(interval);
+        document.getElementById('title').innerHTML = "<span class='blink'>B</span><span class='blink'>R</span>A<span class='blink'>Z</span>ILIAN STORM"
+      }
 
       iterations += 1;
 
