46839f97bfb2196c9d83fab8c7e588bb004a5197
msscsh <itsbruno42@gmail.com>
2023-12-18 17:48:52 -0300

    adicionando funções responsáveis pela popup

--- scripts/home.js
+++ scripts/home.js
@@ -38,19 +38,62 @@ function mostrarEsconderPlacarGeral(element) {
   }
 }
 
-function marcarItem(element) {
-    // Remove a classe 'active' de todos os itens
-    var items = document.querySelectorAll('.menu li');
-    items.forEach(function(item) {
-        item.classList.remove('active');
-    });
-
-    // Adiciona a classe 'active' ao item clicado
-    element.classList.add('active');
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
 }
+
+function displayPopup(nickname, points, starplayer, showdown, normalgame, powerleague) {
+  var popup = document.getElementById('popup');
+  popup.style.display = 'grid';
+
+  adicionaValorNoID('nickname', nickname);
+  adicionaValorNoID('points', points);
+  adicionaValorNoID('starplayer', starplayer);
+  adicionaValorNoID('showdown', firularAApresentacao(showdown), true);
+  adicionaValorNoID('normalgame', firularAApresentacao(normalgame), true);
+  adicionaValorNoID('powerleague', firularAApresentacao(powerleague), true);
+}
+
+function adicionaValorNoID(id, value, addComportamentoDeLinha) {
+  document.getElementById(id).innerHTML = value;
+  if(addComportamentoDeLinha)
+    document.getElementById(id).classList.add('line');
+}
+
+function firularAApresentacao(valor) {
+  var partes = valor.split('/');
+  return '<p style="color:green">'+partes[0]+'</p><p>&nbsp;/&nbsp;</p><p style="color:red">'+partes[1]+'</p>'
+}
+
+function marcarLinksNaCelulaNickname() {
+  var cells = document.querySelectorAll('#ranking td:nth-child(3)');
+  cells.forEach(function(cell) {
+      cell.addEventListener('click', function() {
+        document.getElementById('popup').style.display = 'grid';
+      });
+  });
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
   setInterval(embaralharLetras, 4000);
+
+  var rows = document.querySelectorAll('#ranking tbody tr');
+
+  rows.forEach(function(row) {
+      var nickCell = row.cells[2];
+      nickCell.addEventListener('click', function() {
+          var nickname = row.cells[2].textContent;
+          var points = row.cells[3].textContent;
+          var starplayer = row.cells[4].textContent;
+          var showdown = row.cells[5].textContent;
+          var normalgame = row.cells[6].textContent;
+          var powerleague = row.cells[7].textContent;
+          displayPopup(nickname, points, starplayer, showdown, normalgame, powerleague);
+      });
+  });
+
 });
 
 embaralharLetras();
+// marcarLinksNaCelulaNickname();
